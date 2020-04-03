c1aa1496f2937347536db250f5e2a5b863552d43
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-04 03:17:29 +0530

    Adding Bean Validation

--- src/main/java/io/yadnyesh/yadbrewery/model/BeerDTO.java
+++ src/main/java/io/yadnyesh/yadbrewery/model/BeerDTO.java
@@ -1,10 +1,15 @@
 package io.yadnyesh.yadbrewery.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +19,33 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
+	@Null
 	private UUID id;
+	
+	@Null
 	private Integer version;
+	
+	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
 	private OffsetDateTime createdDate;
+	
+	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
 	private OffsetDateTime lastModifiedDate;
+	
+	@NotBlank
 	private String beerName;
-	private BeerStyleEnum beerStyleEnum;
-	private Long upc;
+	
+	@NotNull
+	private BeerStyleEnum beerStyle;
+	
+	@NotNull
+	private String upc;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@Positive
+	@NotNull
 	private BigDecimal price;
+	
 	private Integer quantityOnHand;
 }
